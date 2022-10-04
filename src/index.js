342189f05e7113e52efb7bf9cbdcd22b99233dd3
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-04 21:47:56 +0500

    Set up Style Defaults

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import store from './Redux/configureStore';
+import './style.css';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
