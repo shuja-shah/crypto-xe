ca731a2507b3f6420a6b85299867c943f35f3bdd
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-04 13:12:54 +0500

    Define Asynchornous Thunk

---
+++ src/Compononenets/Home.js
@@ -0,0 +1,1 @@
+import { getCoins } from "../Redux/coins/coins";
--- src/Redux/coins/coins.js
+++ src/Redux/coins/coins.js
@@ -1,3 +1,5 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
 const GET_COINS = 'crypto-xe/coins/GET_COINS';
 
 const initialState = {
@@ -6,11 +8,21 @@ const initialState = {
     error: null
 };
 
-const getCoins = () => {
-    return {
-        type: GET_COINS
-    };
-}
+const getCoins = createAsyncThunk(GET_COINS, async (args, { dispatch }) => {
+    const response = await fetch('https://coinranking1.p.rapidapi.com/coins' , {
+        "method": "GET",
+        "headers": {
+            'X-RapidAPI-Key': '9f6e02d2f6mshba9a19a6dd2a840p150e78jsn842e700d6d4b',
+            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+        }
+    });
+    const data = await response.json();
+        dispatch({
+            type: GET_COINS,
+            payload: data.data.coins,
+        });
+    });
+    
 export { getCoins };
 
 export default function coinReducer(state = initialState, action) {
@@ -18,8 +30,9 @@ export default function coinReducer(state = initialState, action) {
         case GET_COINS:
             return {
                 ...state,
-                loading: true
+                coins: action.payload,
             };
+
         default:
             return state;
     }
