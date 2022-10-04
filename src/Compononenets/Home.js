d911d9034b391f813e965bac7e3b89ac20e44dfb
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-04 13:13:57 +0500

    Test API

--- src/Compononenets/Home.js
+++ src/Compononenets/Home.js
@@ -1,1 +1,27 @@
 import { getCoins } from "../Redux/coins/coins";
+
+const Home = () => {
+    const dispatch = useDispatch();
+    const coins = useSelector((state) => state.coins.coins);
+    const loading = useSelector((state) => state.coins.loading);
+    const error = useSelector((state) => state.coins.error);
+
+    useEffect(() => {
+        dispatch(getCoins());
+    }, [dispatch]);
+
+    return (
+        <div>
+            <h1>Home</h1>
+            {loading && <h2>Loading...</h2>}
+            {error && <h2>{error}</h2>}
+            {coins.map((coin) => (
+                <div key={coin.id}>
+                    <h2>{coin.name}</h2>
+                    <p>{coin.symbol}</p>
+                    <p>{coin.price}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
