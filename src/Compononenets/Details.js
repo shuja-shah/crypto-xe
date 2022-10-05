ba5a1d0456790ce958b2f72ce9c67a791a6429f4
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-05 18:24:54 +0500

    Define Details Componenet

--- src/Compononenets/Details.js
+++ src/Compononenets/Details.js
@@ -1,4 +1,31 @@
-const Details = () => (
-  <h1>This is the details Page.</h1>
-);
+import { useSelector } from 'react-redux';
+
+const Details = () => {
+  const coinDetails = useSelector((state) => state.details.details.coin);
+  const loading = useSelector((state) => state.details.loading);
+  const price = parseInt(coinDetails.price, 10).toFixed(4).toString();
+  /* remove price and name from coinDetails */
+  const { price: _, name: __, ...rest } = coinDetails;
+  return (
+    <>
+      <section className="Details">
+        {loading && <p>Loading...</p>}
+        <img className="icon" src={coinDetails.iconUrl} alt={coinDetails.name} />
+        <div>
+          <h1>{coinDetails.name}</h1>
+          <p>{price}</p>
+        </div>
+      </section>
+
+      <ul className="detailsList">
+        {Object.entries(rest).map(([key, value]) => (
+          <li key={key}>
+            <span>{key}</span>
+            <span>{value}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 export default Details;
