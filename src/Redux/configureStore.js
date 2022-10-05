4cb63666ecb6cc42da15234e1371fa39773b3741
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-05 16:07:27 +0500

    Combine Reducers

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,6 +2,7 @@ import { combineReducers, configureStore, applyMiddleware } from '@reduxjs/toolk
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import coinsReducer from './coins/coins';
+import detailsReducer from './details/details';
 
 const persistConfig = {
   key: 'main-root',
@@ -10,6 +11,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   coins: coinsReducer,
+  details: detailsReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
