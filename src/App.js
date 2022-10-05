afcda413d924778b9c157186c164a63ec1f8577a
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-05 16:58:27 +0500

    Add Routing Functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Compononenets/Home';
+import Details from './Compononenets/Details';
 
 export default function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/details">
+          <Details />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
