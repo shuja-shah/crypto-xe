7f3e583eaa1c5dce5752827473fee84803c01292
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-05 13:00:35 +0500

    Add Persist

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,12 +1,24 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import coinsReducer from './coins/coins';
+
+const persistConfig = {
+  key: 'main-root',
+  storage,
+};
 
 const rootReducer = combineReducers({
   coins: coinsReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
-});
+  reducer: persistedReducer,
+}, applyMiddleware);
+
+const persistor = persistStore(store);
+export { persistor };
 
 export default store;
