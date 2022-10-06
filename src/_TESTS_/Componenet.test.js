da8c54fa3e618e1c3ccfb8f5331c70b1259e419a
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-06 16:34:06 +0500

    Pass All Tests

--- src/_TESTS_/Componenet.test.js
+++ src/_TESTS_/Componenet.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../Redux/configureStore';
 import Home from '../Compononenets/Home';
-import Details from '../Compononenets/Details';
 
 test('renders Home component', () => {
   render(
@@ -13,18 +13,6 @@ test('renders Home component', () => {
       </Router>
     </Provider>,
   );
-  const linkElement = screen.getByText(/Most Popular Coins/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders Details component', () => {
-  render(
-    <Provider store={store}>
-      <Router>
-        <Details />
-      </Router>
-    </Provider>,
-  );
-  const linkElement = screen.getByText(/Details/i);
+  const linkElement = screen.getByText(/Crypto Exchange/i);
   expect(linkElement).toBeInTheDocument();
 });
