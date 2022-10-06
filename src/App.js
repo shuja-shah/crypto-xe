c5297f0adfd3ba2adb0eecbd6954abdeba1600d6
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-06 16:45:48 +0500

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Compononenets/Header';
 import Home from './Compononenets/Home';
 import Details from './Compononenets/Details';
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/">
           <Home />
